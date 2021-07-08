5f5267cdad7a48ef5fbde2e3530c0c191583f59b
Welberth54 <edinaldoramos380p@gmail.com>
2021-07-08 10:10:42 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,5 @@
+const config = {
+        botName: 'WELBERTH_BOT',
+        ownerName: 'Welberth',
+        instagram: 'https://Instagram.com/welberth._ofc',
+}
---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const Welberthmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Welberth*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.Welberthmenu= botWelberthmenu
